d1f36a6b950f525d40105f712b7f7ea1f830501c
joecover99 <joecover99@Joecover.home>
2019-10-05 22:14:11 -0400

    Infirmary created, new tests

--- TP1_DuelFighter/src/exception/fighter/IllegalAbilityValueException.java
+++ TP1_DuelFighter/src/exception/fighter/IllegalAbilityValueException.java
@@ -3,9 +3,10 @@ package exception.fighter;
 	public class IllegalAbilityValueException extends IllegalArgumentException{
 		public static final String WRONG_WARRIOR_STATS = "The stats you asked for are not valid for a warrior class";
 		public static final String WRONG_BASE_STATS = "The stats you asked for are not valid for any class";
-		public static final String WRONG_MAGE_STATS = "The stats you asked for are not valid for a mage class";
 		public static final String WRONG_ATHLETE_STATS = "The stats you asked for are not valid for an athlete class";
+		public static final String WRONG_MAGE_STATS = "The stats you asked for are not valid for a mage class";
 		public static final String TOO_LOW_ABILITY_STATS = "The ability value can't be lower than zero";
+
 		
 		public IllegalAbilityValueException(String s) {
 			super(s);
